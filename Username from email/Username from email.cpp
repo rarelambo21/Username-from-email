f1de3dbfa3f79868263b50f0ff1d5b95d353790e
rarelambo21 <35502394+rarelambo21@users.noreply.github.com>
2021-03-02 11:23:42 +0300

    Add project files.

---
+++ Username from email/Username from email.cpp
@@ -0,0 +1,12 @@
+// Username from email.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
